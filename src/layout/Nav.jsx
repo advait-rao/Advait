3442251ab9ba4f90258575ad0f333f3ff3b18d2d
Addy Rao <addy.rao@xero.com>
2021-12-27 15:09:30 +1300

    thumbnails and navbar zindex

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -8,57 +8,47 @@ export const Nav = () => {
         <Box
             className='navbar'
             sx={{
-                width: '100%',
-                minHeight: '5vh',
+                fontFamily: 'Source Code Pro',
+                fontSize: '2.1vh',
+                paddingTop: '1.5vh',
+                paddingBottom: '2vh',
                 backgroundColor: '#061324',
                 display: 'flex',
                 flexDirection: 'row',
                 position: 'fixed',
+                width: '100%',
+                zIndex: '100',
             }}
         >
-            <Box
-                sx={{
-                    paddingRight: '2rem',
-                    color: '#6F2232',
-                    fontFamily: 'Source Code Pro',
-                    fontSize: '2.1vh',
-                    paddingTop: '1rem',
-                    paddingBottom: '0.5rem',
-                }}
+            <Stack
+                direction='row'
+                divider={
+                    <Divider orientation='vertical' flexItem color='#4E4E50' />
+                }
+                spacing={2}
+                sx={{}}
             >
-                <Stack
-                    direction='row'
-                    divider={
-                        <Divider
-                            orientation='vertical'
-                            flexItem
-                            color='#4E4E50'
-                        />
-                    }
-                    spacing={2}
-                >
-                    <li className='Nav' style={{ listStyleType: 'none' }}>
-                        <a href='#home' className='navlink'>
-                            Home
-                        </a>
-                    </li>
-                    <li className='Nav' style={{ listStyleType: 'none' }}>
-                        <a href='#about' className='navlink'>
-                            About
-                        </a>
-                    </li>
-                    <li className='Nav' style={{ listStyleType: 'none' }}>
-                        <a href='#projects' className='navlink'>
-                            Projects
-                        </a>
-                    </li>
-                    <li className='Nav' style={{ listStyleType: 'none' }}>
-                        <a href='#contact' className='navlink'>
-                            Contact
-                        </a>
-                    </li>
-                </Stack>
-            </Box>
+                <li className='Nav' style={{ listStyleType: 'none' }}>
+                    <a href='#home' className='navlink'>
+                        Home
+                    </a>
+                </li>
+                <li className='Nav' style={{ listStyleType: 'none' }}>
+                    <a href='#about' className='navlink'>
+                        About
+                    </a>
+                </li>
+                <li className='Nav' style={{ listStyleType: 'none' }}>
+                    <a href='#projects' className='navlink'>
+                        Projects
+                    </a>
+                </li>
+                <li className='Nav' style={{ listStyleType: 'none' }}>
+                    <a href='#contact' className='navlink'>
+                        Contact
+                    </a>
+                </li>
+            </Stack>
         </Box>
     );
 };
